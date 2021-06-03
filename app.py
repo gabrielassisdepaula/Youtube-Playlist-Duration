3a6d81520fb424384bc9f1c79611cfaa1c5d1c45
Gabriel Assis <gabriel.ifood.com.br>
2021-06-03 17:18:15 -0300

    Created secret folder and updated gitignore

--- app.py
+++
@@ -1,9 +0,0 @@
-import argparse
-from PlaylistDuration import PlaylistDuration
-
-ap = argparse.ArgumentParser()
-ap.add_argument('-u', '--url', required=False, help="URL Path")
-args = vars(ap.parse_args())
-my_url = args['url']
-
-PlaylistDuration(my_url)
